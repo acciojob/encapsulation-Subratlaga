f8837ae28bd4e020f73fc065e98d7aea9c1b8fda
Subratlaga <126932755+Subratlaga@users.noreply.github.com>
2026-07-24 20:53:12 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class RWOnly {
-    
+   
+    // Private member variable
+    private String name;
+
+    // Setter method
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter method
+    public String getName() {
+        return name;
+    }
+
 }
