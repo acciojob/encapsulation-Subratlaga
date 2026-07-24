2d7ff819cba6735a89931763ef29e3175ffbd1b5
Subratlaga <126932755+Subratlaga@users.noreply.github.com>
2026-07-24 20:52:41 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,41 +3,27 @@ package com.driver;
 
   public class Main {
 
+
     public static void main(String[] args) {
 
         // Create object of RWOnly
         RWOnly obj = new RWOnly();
 
-        // Direct access (This will give compile-time errors)
-        // obj.name = "Subrat";
+        // Direct access (This will cause compile-time errors)
+
+        // obj.name = "John";
         // System.out.println(obj.name);
 
         /*
          * Error:
          * name has private access in RWOnly
+         * The field RWOnly.name is not visible
          */
 
-        // Using Setter
-        obj.setName("Subrat");
+        // Set value using setter
+        obj.setName("John");
 
-        // Using Getter
-        System.out.println("Name: " + obj.getName());
-    }
-}
-
-// RWOnly class
-class RWOnly {
-
-    // Private member variable
-    private String name;
-
-    // Setter Method
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    // Getter Method
-    public String getName() {
-        return name;
+        // Get value using getter
+        System.out.println(obj.getName());
     }
 }
